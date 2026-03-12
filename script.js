a08b36dad84c49a3ef20081deedf175863a7256b
Patrick T. Carroll <carrolpt@dukes.jmu.edu>
2026-03-12 10:13:48 -0400

    finished javascript

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ let contact = document.getElementById("contact");
 contact.addEventListener("click", sayHello);
 
 function sayHello() {
-  alert("Under Construction - Thanks for visiting my profile!");
+  let email = document.getElementById("email");
+
+  if (email.style.display === "block") {
+    email.style.display = "none";
+    contact.textContent = "Say Hello!";
+  } else {
+    email.textContent = "📧 ptcarroll1@gmail.com";
+    email.style.display = "block";
+    email.style.backgroundColor = "yellow";
+    contact.textContent = "Hide!";
+  }
 }
